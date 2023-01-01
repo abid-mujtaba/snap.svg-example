c3ca8d53064504c133296160d29d34e9a8b8037f
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2023-01-01 17:16:17 -0500

    Install snapsvg to package.json and the webpack config both

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,18 @@ module.exports = {
     },
     output: {
         filename: '[name].bundle.js'
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: require.resolve('snapsvg/dist/snap.svg.js'),
+                use: 'imports-loader?this=>window,fix=>module.exports=0',
+            },
+        ],
+    },
+    resolve: {
+        alias: {
+            snapsvg: 'snapsvg/dist/snap.svg.js',
+        },
+    },
 }
