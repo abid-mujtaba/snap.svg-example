d2dc90a3b2063aa8aab1e9d5a4af3c6ee59096af
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2023-01-01 18:41:23 -0500

    Working initial version (using window.onload)

--- src/entry.js
+++ src/entry.js
@@ -1,1 +1,8 @@
-console.log("Hello, World! From webpack.");
+import Snap from "snapsvg";
+
+window.onload = async () => {
+    const s = Snap("#svg");
+    s.circle(150, 150, 100);
+};
+
+window.onunload = () => { };
