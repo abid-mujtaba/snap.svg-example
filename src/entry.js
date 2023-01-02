11c4f400790549cbbcbbbc42343a7ae0b20ffaf4
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2023-01-01 19:44:09 -0500

    Test animation

--- src/entry.js
+++ src/entry.js
@@ -2,7 +2,13 @@ import Snap from "snapsvg";
 
 window.onload = async () => {
     const s = Snap("#svg");
-    s.circle(150, 150, 100);
+    const c1 = s.circle(150, 150, 100);
+    const c2 = s.circle(600, 150, 50);
+
+    c2.attr({ fill: "green" });
+
+    c1.animate({ r: 50 }, 2000);
+    c2.animate({ r: 100 }, 2000);
 };
 
 window.onunload = () => { };
