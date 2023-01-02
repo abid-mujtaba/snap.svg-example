4def06b69ed2e5aec3c4722a47114655b369be70
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2023-01-01 19:48:31 -0500

    Refactor to put core functionality in separate module (from entry)

---
+++ src/core.js
@@ -0,0 +1,15 @@
+import Snap from "snapsvg";
+
+export { main };
+
+async function main() {
+    const s = Snap("#svg");
+    const c1 = s.circle(150, 150, 100);
+    const c2 = s.circle(600, 150, 50);
+
+    c2.attr({ fill: "green" });
+
+    c1.animate({ r: 50 }, 2000);
+    c2.animate({ r: 100 }, 2000);
+}
+
