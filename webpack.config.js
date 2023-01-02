089afe04267dc8b014cf5b77eadd06368948e21a
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2023-01-01 20:45:48 -0500

    Switch to using typescript (allows updating to webpack 4)

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ const webpack = require('webpack');
 
 module.exports = {
     entry: {
-        entry: __dirname + '/src/entry.js'
+        entry: __dirname + '/src/entry.ts'
     },
     output: {
-        filename: 'dist/[name].bundle.js'
+        filename: '[name].bundle.js'
     },
     module: {
         rules: [
@@ -13,11 +13,17 @@ module.exports = {
                 test: require.resolve('snapsvg/dist/snap.svg.js'),
                 use: 'imports-loader?this=>window,fix=>module.exports=0',
             },
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
         ],
     },
     resolve: {
         alias: {
             snapsvg: 'snapsvg/dist/snap.svg.js',
         },
+        extensions: ['.tsx', '.ts', '.js']
     },
 }
